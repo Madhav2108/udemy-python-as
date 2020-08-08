0aad0a4c0bb1b96fb3b2aaa85a6506a5deba2584
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-08-08 17:13:51 +0530

    Add files via upload

---
+++ basic/inputopret.py
@@ -0,0 +1,16 @@
+import operator
+a=10
+b=20
+print(operator.add(a,b))
+print(operator.sub(b,a))
+print(operator.mul(a,b))
+print(operator.mod(a,b))
+print(operator.pow(a,b))
+print(operator.floordiv(a,b))
+print(operator.truediv(a,b))
+print(operator.lt(a,b))
+print(operator.le(a,b))
+print(operator.eq(a,b))
+print(operator.gt(a,b))
+print(operator.ge(a,b))
+print(operator.ne(a,b))
---
+++ basic/listadd.py
@@ -0,0 +1,9 @@
+list=[1,2,3,4,5,6,7,8,9]
+print(list)
+list.append(11)
+print(list)
+list.pop()
+print(list)
+
+for i in range(0,10):
+    print (i)
