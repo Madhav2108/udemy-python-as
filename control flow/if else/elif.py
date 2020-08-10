41af3b25382c010b57b8a585eb144a82e69608a7
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-08-10 23:13:00 +0530

    Add files via upload

---
+++ control flow/if else/elif.py
@@ -0,0 +1,10 @@
+print("ENTER THE VALUE")
+i = input()
+if (i == '10'): 
+    print ("i is 10") 
+elif (i == '15'): 
+    print ("i is 15") 
+elif (i == '20'): 
+    print ("i is 20") 
+else: 
+    print ("i is not present")
