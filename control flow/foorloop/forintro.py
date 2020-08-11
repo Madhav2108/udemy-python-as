00e45bde4e41782e7bccfa8f741bd7bc74d600cb
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-08-11 22:18:49 +0530

    Add files via upload

---
+++ control flow/foorloop/forintro.py
@@ -0,0 +1,8 @@
+s = "Madhav"
+for i in s :  
+    print(i)
+d = dict()   
+d['h2so4'] = 21
+d['vulture'] = 1999
+for i in d :  
+    print("% s % d" %(i, d[i]))
---
+++ control flow/foorloop/searchloop.py
@@ -0,0 +1,9 @@
+for l in 'RiFtVulture':  
+    if l == 'R' or l == 'l':  
+        continue
+    print('-', l, end=" ")
+print("\n-------------------------------------------------------------------------")
+for l in 'RiFtVulture':  
+    if l == 'V' or l == 't':  
+        break
+    print('-', l, end=" ")
