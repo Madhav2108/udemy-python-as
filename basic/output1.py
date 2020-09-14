4bad4d0a1195860b9f506cd701c02aa732306c92
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-15 02:53:57 +0530

    Add files via upload

---
+++ basic/output1.py
@@ -0,0 +1,105 @@
+
+"""
+bASIC aLL
+"""
+print('A\nB\nC')
+print('D\tE\tF')
+print('WX\bYZ')
+print('1\a2\a3\a4\a5\a6')
+x = 6
+print(6)
+print("6")
+x = 7
+print(x)
+print("x")
+
+w, x, y, z = 10, 15, 20, 25
+print(w, x, y, z)
+print(w, x, y, z, sep=',')
+print(w, x, y, z, sep='')
+print(w, x, y, z, sep=':')
+print(w, x, y, z, sep='-----')
+
+print('A', end='')
+print('B', end='')
+print('C', end='')
+print()
+print('X')
+print('Y')
+print('Z')
+
+"""Listing 2.15: evalfunc.py
+x1 = eval(input('Entry x1? '))
+print('x1 =', x1, ' type:', type(x1))
+x2 = eval(input('Entry x2? '))
+print('x2 =', x2, ' type:', type(x2))
+x3 = eval(input('Entry x3? '))
+print('x3 =', x3, ' type:', type(x3))
+x4 = eval(input('Entry x4? '))
+print('x4 =', x4, ' type:', type(x4))
+x5 = eval(input('Entry x5? '))
+print('x5 =', x5, ' type:', type(x5))"""
+
+x = 2
+print("x")
+print('x')
+print(x)
+print("x + 1")
+#print('x'+ 1) Invalid
+print(x + 1)
+
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5;
+print(i1 + i2)
+print(i1 / i2)
+print(i1 // i2)
+print(i2 / i1)
+print(i2 // i1)
+print(i1 * i3)
+print(d1 + d2)
+print(d1 / d2)
+print(d2 / d1)
+print(d3 * d1)
+print(d1 + i2)
+print(i1 / d2)
+print(d2 / i1)
+print(i2 / d1)
+print(i1/i2*d1)
+print(d1*i1/i2)
+print(d1/d2*i1)
+print(i1*d1/d2)
+print(i2/i1*d1)
+print(d1*i2/i1)
+print(d2/d1*i1)
+print(i1*d2/d1)
+
+
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+print(i1 + (i2 * i3))
+print(i1 * (i2 + i3))
+print(i1 / (i2 + i3))
+print(i1 // (i2 + i3))
+print(i1 / i2 + i3)
+print(i1 // i2 + i3)
+print(3 + 4 + 5 / 3)
+print(3 + 4 + 5 // 3)
+print( (3 + 4 + 5) / 3)
+print( (3 + 4 + 5) // 3)
+print(d1 + (d2 * d3))
+print(d1 + d2 * d3)
+print(d1 / d2 - d3)
+print(d1 / (d2 - d3))
+print(d1 + d2 + d3 / 3)
+print( (d1 + d2 + d3) / 3)
+print(d1 + d2 + (d3 / 3))
+print(3 * (d1 + d2) * (d1 - d3))
+
