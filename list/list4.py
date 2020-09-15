7be793ac75ee9bcec1ca605da8441909589235ff
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-16 00:07:19 +0530

    Add files via upload

---
+++ list/list4.py
@@ -0,0 +1,20 @@
+print("Program :4 :")
+# slice lists 
+my_list = ['p','r','o','g','r','a','m','m','i','n','g']
+print(my_list)
+# elements 3rd to 5th
+print(my_list[2:5])
+# elements beginning to 4th
+print(my_list[:-5])
+# elements 6th to end
+print(my_list[5:])
+# elements beginning to end
+print(my_list[:])
+"""
+Program :4 :
+['p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g']
+['o', 'g', 'r']
+['p', 'r', 'o', 'g', 'r', 'a']
+['a', 'm', 'm', 'i', 'n', 'g']
+['p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g']
+"""
