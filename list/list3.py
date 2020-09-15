c7c0e3a73891b2f85197497cee9bbc3486925ab7
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-16 00:07:05 +0530

    Add files via upload

---
+++ list/list3.py
@@ -0,0 +1,19 @@
+print("program :3 : ")
+# Nested List
+n_list = ["Happy", [2,0,1,5]]
+print(n_list)
+print(n_list[0])
+print(n_list[1])
+print(n_list[0][4])
+print(n_list[1][0])
+print(n_list[1][3])
+print(n_list[1][4])
+"""
+program :3 : 
+['Happy', [2, 0, 1, 5]]
+Happy
+[2, 0, 1, 5]
+y
+2
+5
+"""
