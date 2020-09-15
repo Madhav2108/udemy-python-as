7e7dbf4da51004d2dd63f9655ee14b655727aeae
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-16 00:04:36 +0530

    Add files via upload

---
+++ list/append_method.py
@@ -0,0 +1,20 @@
+# animal list
+animal = ['cat', 'dog', 'rabbit']
+
+# an element is added
+animal.append('guinea pig ')
+
+#Updated Animal List
+print('Updated animal list: ', animal)
+
+# animal list
+animal = ['cat', 'dog', 'rabbit']
+
+# another list of wild animals
+wild_animal = ['tiger', 'fox']
+
+# adding wild_animal list to animal list
+animal.append(wild_animal)
+
+#Updated List
+print('Updated animal list: ', animal)
