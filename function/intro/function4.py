fa3bbab8a33c8be4901dad88a22fa234dae8c4ba
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-17 02:27:59 +0530

    Add files via upload

---
+++ function/intro/function4.py
@@ -0,0 +1,27 @@
+def add(a,b):
+    c=a+b
+    print(c)
+def sub(a,b):
+    c=a-b
+    print(c)
+def mul(a,b):
+    c=a*b
+    print(c)
+def div(a,b):
+    c=a/b
+    print(c)
+
+a=int(input("Enter Value of a: "))
+b=int(input("Enter Value of b: "))
+n=int(input("1 for add,2 for sub,3 for div,4 for mul: "))
+if n==1:
+    add(a,b)
+elif n==2:
+    sub(a,b)
+elif n==3:
+    div(a,b)
+elif n==4:
+    mul(a,b)
+else:
+    print("Not a Valid Input")
+
