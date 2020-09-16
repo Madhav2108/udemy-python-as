c9e8f7d113e24fb53fefbb09331543600a2c5de6
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-17 02:27:44 +0530

    Add files via upload

---
+++ function/intro/function3.py
@@ -0,0 +1,27 @@
+def add(a,b):
+    c=a+b
+    print(c)
+def sub(a,b):
+    c=a-b
+    print(c)
+def mul(a,b):
+    c=a*b
+    print(c)
+def div(a,b):
+    c=a/b
+    print(c)
+
+a=int(input("Enter Value of a: "))
+b=int(input("Enter Value of b: "))   
+add(a,b)
+mul(a,b)
+div(a,b)
+sub(a,b)
+"""
+Enter Value of a: 5
+Enter Value of b: 6
+11
+30
+0.8333333333333334
+-1
+"""
