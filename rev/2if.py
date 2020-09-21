15f2b5ffa96f5b36e99ebac168e2dceb5da23611
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-21 19:04:36 +0530

    Add files via upload

---
+++ rev/2if.py
@@ -0,0 +1,11 @@
+Python 3.3.2 (v3.3.2:d047928ae3f6, May 16 2013, 00:06:53) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> P=int(input("Enter the Percentage"))
+if P<40:
+    print("you are failed")
+elif P==40 to 50:
+    print("You are Third")
+elif P==50 to 60:
+    print("You are Second")
+else:
+    print("you are First")
