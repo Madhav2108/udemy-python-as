f7bfa84cba19d63b0bc6d049fb509f00d4038c07
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-27 17:05:14 +0530

    Add files via upload

---
+++ basic/el23.py
@@ -0,0 +1,14 @@
+x="Gajendra"
+#print(x[0])
+for i in range(0,x.__len__()+1,1):
+    for j in range(0,i,1):
+        print(x[j],end="")
+    print()    
+        
+            
+            
+    
+       
+        
+         
+    
