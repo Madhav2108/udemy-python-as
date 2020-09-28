540a6d21794b53d9027d02505a3d4040eff4142d
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-28 22:44:31 +0530

    Add files via upload

---
+++ basic/juj.py
@@ -0,0 +1,11 @@
+n=int(input("enter value of n:"))
+if n>=95:
+    print("division is first")
+elif 90<n<80:
+    print("division is second")
+elif 80<n<70:
+    print ("division is third")
+elif n<33:
+    print("failed")
+else:
+    print("passed")
