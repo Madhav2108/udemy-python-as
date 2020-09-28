c5d7c3c26db1d45cbbe7f2ecafb07dc68ff33f79
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-28 22:42:10 +0530

    Add files via upload

---
+++ basic/hiprint1.py
@@ -0,0 +1,10 @@
+#Set 1, Prog 1
+print("Set 1, Prog 1")
+for i in range(1,16,1):
+     print("i= ",i)
+#Set 1, Prog 2
+print("Set 1, Prog 2")
+sum=0
+for i in range(1,16,1):
+     sum=sum+i
+print("Sum= ",sum)
