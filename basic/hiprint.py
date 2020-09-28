d10551c1d3f42afb3b75fce765f9ae7f739a925a
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-28 22:41:55 +0530

    Add files via upload

---
+++ basic/hiprint.py
@@ -0,0 +1,11 @@
+for i in range(1,10+1,2):
+    print (i,"Hi")
+for i in [1,4,7,9]:
+    print (i,"Hi")
+n=int(input("Enter any Number :" ))
+for i in range(1,11,1):
+    print (i," * ",n," = ",i*n)
+for i in range(1,11,1):
+    print (i*n)
+for ravi in range(1,11,1):
+    print (ravi*n)
