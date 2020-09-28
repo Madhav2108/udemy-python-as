cf2de6fae8c8cedbb50e7566bd673757c8e1c1b7
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-28 22:43:26 +0530

    Add files via upload

---
+++ basic/ifgrater.py
@@ -0,0 +1,10 @@
+N=int(input("Enter Value of N: "))
+if N%2==0 and N%5==0:
+    print("Yes")
+else:
+    print("No")
+N=int(input("Enter Value of N: "))
+if N%2==0 or N%5==0:
+    print("Yes")
+else:
+    print("No")
