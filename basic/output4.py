b78b569218ffd889603e662e7b48221adeb5c429
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-29 21:31:11 +0530

    Add files via upload

---
+++ basic/output4.py
@@ -0,0 +1,22 @@
+"""bASIC 3
+"""
+print(eval(input()))
+print('A', end='')
+print('B', end='')
+print('C', end='')
+print()
+print('X')
+print('Y')
+print('Z')
+w, x, y, z = 10, 15, 20, 25
+print(w, x, y, z)
+print(w, x, y, z, sep=',')
+print(w, x, y, z, sep='')
+print(w, x, y, z, sep=':')
+print(w, x, y, z, sep='-----')
+x = 6
+print(6)
+print("6")
+x = 7
+print(x)
+print("x")
