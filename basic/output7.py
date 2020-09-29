2b735e1df089146d2be3c1bb2c8736f7d0d5e18b
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-29 21:34:02 +0530

    Add files via upload

---
+++ basic/output7.py
@@ -0,0 +1,32 @@
+b1=1
+b2=0
+x=3
+y=5
+b3=x==3
+b4=y<3
+print(b3)
+print(b4)
+print(not b1)
+print(not b2)
+print(not b3)
+print(not b4)
+print(b1 and b2)
+print(b1 or b2)
+print(b1 and b3)
+print(b1 or b3)
+print(b1 and b4)
+print(b1 or b4)
+print(b2 and b3)
+print(b2 or b3)
+print(b1 and b2 or b3)
+print(b1 or b2 and b3)
+print(b1 and b2 and b3)
+print(b1 or b2 or b3)
+print(not b1 and b2 and b3)
+print(not b1 or b2 or b3)
+print(not (b1 and b2 and b3))
+print(not (b1 or b2 or b3))
+print(not b1 and not b2 and not b3)
+print(not b1 or not b2 or not b3)
+print(not (not b1 and not b2 and not b3))
+print(not (not b1 or not b2 or not b3))
