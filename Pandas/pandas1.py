0aaa6fbe4daa0147fb53449455678b131bb69afb
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-10 21:46:11 +0530

    Add files via upload

---
+++ Pandas/pandas1.py
@@ -0,0 +1,25 @@
+import pandas as p
+print("Data: \n")
+# data1=p.DataFrame()
+# print(data1)
+# #Output: 
+# #Empty DataFrame
+# # Columns: []
+# # Index: []
+# # List: 
+# print("Single list Type Data: \n")
+# l1=[1,2,3]
+# data2=p.DataFrame(l1)
+# print(data2)
+# print("Nested Type Data: \n")
+# l2=[[1,2,3],[4,5,6]]
+# data3=p.DataFrame(l2)
+# print(data3)
+# print("Multi Type Data: \n")
+# data = [['Alex',10],['Bob',12],['Clarke',13]]
+# df = p.DataFrame(data,columns=['Name','Age'])
+# print (df)
+# print("Float type  Data: \n")
+# data = [['Alex',10],['Bob',12],['Clarke',13]]
+# df = p.DataFrame(data,columns=['Name','Age'],dtype=float)
+# print (df)
