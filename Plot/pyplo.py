397a451ae2eee248e04719bf047d9a7e52ac5d5d
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-11 16:33:02 +0530

    Add files via upload

---
+++ Plot/pyplo.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as p
+p.plot([1,7,3,8,10],[2,4,6,8,20],'y-')
+p.plot([1,7,3,8,10],[2,4,6,8,20],'g^')
+p.ylabel('Y Range')
+p.xlabel('X Range')
+p.title("First Plot")
+p.show()
