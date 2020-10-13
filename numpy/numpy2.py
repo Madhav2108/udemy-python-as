d34c82335074aaa441daf23228cea9680cb5e22d
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-13 14:48:53 +0530

    Add files via upload

--- numpy/numpy2.py
+++ numpy/numpy2.py
@@ -1,16 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Compute the x and y coordinates for points on sine and cosine curves
-x = np.arange(0, 3 * np.pi, 0.1)
-y_sin = np.sin(x)
-y_cos = np.cos(x)
-
-# Plot the points using matplotlib
-plt.plot(x, y_sin)
-plt.plot(x, y_cos)
-plt.xlabel('x axis label')
-plt.ylabel('y axis label')
-plt.title('Sine and Cosine')
-plt.legend(['Sine', 'Cosine'])
-plt.show()
+import numpy as np 
+a = np.array([[1,2],[3,4],[5,6]], ndmin = 1) 
+print (a)
