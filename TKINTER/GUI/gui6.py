0f71d235844896e826d9c638e470904ae7fda257
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-16 19:05:58 +0530

    Add files via upload

---
+++ TKINTER/GUI/gui6.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter.ttk import *
+window = Tk()
+window.title("Welcome to LikeGeeks app")
+window.geometry('350x200')
+combo = Combobox(window)
+combo['values']= (1, 2, 3, 4, 5, "Text")
+combo.current(1) #set the selected item
+combo.grid(column=0, row=0)
+window.mainloop()
+#chk_state = IntVar()
+#chk_state.set(0) #uncheck
+#chk_state.set(1) #check
