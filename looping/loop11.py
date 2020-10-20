3154f36bef941b205a33dab36946f059e425e1ee
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-20 20:40:32 +0530

    Add files via upload

---
+++ looping/loop11.py
@@ -0,0 +1,20 @@
+print("Loop")
+for i in range(1,10+1,1):      #s,e-1,steps):
+    print(i,"HI")
+
+print("Loop")
+for i in range(1,10+1,1):      #s,e-1,steps):
+    print("HI",end="")
+
+print("Table")
+for i in range(1,10+1,1):      #s,e-1,steps):
+    print(i*5)
+
+print("Table of given Number ")
+n=int(input("Enter any number: "))
+for i in range(1,10+1,1):      #s,e-1,steps):
+    print(i,"*",n," = ",i*n)
+    
+print("Loop")
+for i in range(1,10+1,3):      #s,e-1,steps):
+    print(i,"HI")
