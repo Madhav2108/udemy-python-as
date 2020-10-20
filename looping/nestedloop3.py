cc7ecab8c9b0910feb35f1749dd4e48b8b8a9b4b
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-20 20:42:04 +0530

    Add files via upload

---
+++ looping/nestedloop3.py
@@ -0,0 +1,18 @@
+print("Loop Type 10")
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i,end="")
+    print()
+print("Loop Type 11")
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+print("Loop Type 12")
+for i in range(1,6):
+    n=i
+    for j in range(1,i+1):
+        print(n,end="")
+        n=n+1
+    print()
+            
