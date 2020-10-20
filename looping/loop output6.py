5f77f00836729c59dd401b06ad983c22acc4ff66
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-20 20:39:51 +0530

    Add files via upload

---
+++ looping/loop output6.py
@@ -0,0 +1,19 @@
+#while
+i=11
+k=2
+while i>=4:
+    k=k*i
+    print(k,", ",i)
+    i=i-2
+#for 
+k=2
+for i in range(11,4,-2):
+    k=k*i
+    print(k,", ",i)
+#output:
+"""
+22 ,  11
+198 ,  9
+1386 ,  7
+6930 ,  5
+"""
