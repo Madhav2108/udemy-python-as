f46be121155603c83f2f12e207e597d778fa6db2
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-20 20:39:01 +0530

    Add files via upload

---
+++ looping/loop output3.py
@@ -0,0 +1,16 @@
+#while
+i=1
+k=2
+while i<=10:
+    k=k+i
+    print(k)
+    i=i+3
+#Convert in for and find output
+k=2
+for i in range(1,11,3):
+     k=k+i
+     print(k)
+#output:
+"""
+3, 7,14,24
+"""
