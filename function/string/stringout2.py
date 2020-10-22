113188ad9817d806fa607ea3662d5fd2867fd610
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-22 20:13:42 +0530

    Add files via upload

---
+++ function/string/stringout2.py
@@ -0,0 +1,27 @@
+str="Python for All type of Design"
+print(str)
+print(str.endswith("a"))
+print(str.endswith("n"))
+print(str.find("n"))
+print(str.find("o"))
+print(str.find("of"))
+str1="Aman23"
+print(str1.isalnum())
+print(str1.isalpha())
+str2="123@"
+print(str2.isdigit())
+str3="123"
+print(str3.isdigit())
+"""
+Output:
+Python for All type of Design
+False
+True
+5
+4
+20
+True
+False
+False
+True
+"""
