1fe6a8ab717d026532cb500594ac1cea655fda0e
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-22 20:12:39 +0530

    Add files via upload

---
+++ function/string/stringout1.py
@@ -0,0 +1,16 @@
+str="Python for All type of Design"
+print(str) #Python for  All type of Design
+print(str[3]) # h
+print(str[3:10]) #hon for 
+print(str[5:]) #n for  All type of Design
+print(str[-3]) #i
+print(len(str)) #29
+"""
+Output:
+Python for All type of Design
+h
+hon for
+n for All type of Design
+i
+29
+"""
