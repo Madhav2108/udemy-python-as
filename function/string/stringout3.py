dc440ccb883636eb182a08d0e246dd2885f261b1
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-22 20:14:42 +0530

    Add files via upload

---
+++ function/string/stringout3.py
@@ -0,0 +1,25 @@
+str="sAy To hEllo PyThon"
+print(str)
+print(str.islower() )
+print(str.isspace() )
+print(str.istitle() )
+print(str.lower() )
+print(str.upper() )
+print(str.title() )
+print(str.swapcase() )
+print(str.replace("To","from" ))
+print(str.center(30,"*"))
+"""
+Output:
+sAy To hEllo PyThon
+False
+False
+False
+say to hello python
+SAY TO HELLO PYTHON
+Say To Hello Python
+SaY tO HeLLO pYtHON
+sAy from hEllo PyThon
+*****sAy To hEllo PyThon******
+
+"""
