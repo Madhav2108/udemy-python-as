5d6033f5e72e5c556d9e19f611f59a97311a2c0c
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-12-06 17:22:56 +0530

    Add files via upload

---
+++ function/first class/fun (2).py
@@ -0,0 +1,14 @@
+#functions
+#default
+def dis():          #1
+    print("I am a function..")
+def dis1(a):        #2
+    print(a)
+def dis2(a,b):      #3
+    print(a+b)
+
+dis()               #1
+dis1(5)             #2
+dis2(4,5)           #3
+dis1("Python")      #2
+dis2("data",".txt") #3
