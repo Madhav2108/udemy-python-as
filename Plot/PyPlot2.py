dd06e6177ae99e03e83058b54c2a63f9348dc025
Madhav Narayan <madhav1471.cse18@chitkara.edu.in>
2021-01-02 08:25:35 +0530

    Add files via upload

---
+++ Plot/PyPlot2.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as p
+
+x=[i for i in range(10)]
+y=[i*2 for i in x]
+p.plot(x,y,'k--')
+p.plot(x,y,'go')
+p.ylabel('Y RANGE')
+p.xlabel('X RANGE')
+p.title("FIRST PLOT")
+p.show()
